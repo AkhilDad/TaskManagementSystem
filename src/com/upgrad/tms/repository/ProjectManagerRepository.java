00c69c4c20e9d83b5bcde86a7f384b7f7af9aadd
Akhil Dad <dadakhil@gmail.com>
2020-02-01 10:47:53 +0530

    Remove collection from ProjectManagerRepository

--- src/com/upgrad/tms/repository/ProjectManagerRepository.java
+++ src/com/upgrad/tms/repository/ProjectManagerRepository.java
@@ -3,12 +3,13 @@ package com.upgrad.tms.repository;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class ProjectManagerRepository {
 
     private static ProjectManagerRepository managerRepository;
-    private HashMap<String, String> managerCredentials;
+    private String usernameList[];
+    private String passwordList[];
+    private int nextEmptyIndex;
 
     public static ProjectManagerRepository getInstance() {
         if (managerRepository == null) {
@@ -18,18 +19,22 @@ public class ProjectManagerRepository {
     }
 
     private ProjectManagerRepository() {
+        nextEmptyIndex = 0;
         initManagerCredentials();
     }
 
     private void initManagerCredentials() {
-        managerCredentials = new HashMap<>();
+        usernameList = new String[10];
+        passwordList = new String[10];
         BufferedReader br;
         try {
             br = new BufferedReader(new FileReader("manager.txt"));
             String line;
             while ((line = br.readLine()) != null) {
                 String[] split = line.split(",");
-                managerCredentials.put(split[0], split[1]);
+                usernameList[nextEmptyIndex] = split[0];
+                passwordList[nextEmptyIndex] = split[1];
+                nextEmptyIndex++;
             }
             br.close();
         } catch (IOException e) {
@@ -39,10 +44,24 @@ public class ProjectManagerRepository {
     }
 
     public boolean isManager(String username) {
-        return managerCredentials.containsKey(username);
+        boolean isUsernameExists = false;
+        for (int i = 0; i < nextEmptyIndex; i++) {
+            isUsernameExists = usernameList[i].equals(username);
+            if (isUsernameExists) {
+                break;
+            }
+        }
+        return isUsernameExists;
     }
 
     public boolean isValidManagerCredentials(String username, String password) {
-        return managerCredentials.containsKey(username) && managerCredentials.get(username).equals(password);
+        boolean isValidCredentials = false;
+        for (int i = 0; i < nextEmptyIndex; i++) {
+            isValidCredentials = usernameList[i].equals(username) && passwordList[i].equals(password);
+            if (isValidCredentials) {
+                break;
+            }
+        }
+        return isValidCredentials;
     }
 }
